e89809dee00822eb9a13a1a81f3ee9471615c13d
vedransehovic <vedran.sehovic@gmail.com>
2020-11-08 19:54:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const testVar = {}
+const testVar = 
+[
+  {year: "2018", result: "L"},
+  //{year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+];
+
 
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let resultObj, result;
+    resultObj = array.find(o => o.result === 'W');
+    if (resultObj !== undefined) {
+      result = resultObj.year;
+    }
+    return result;
+};
